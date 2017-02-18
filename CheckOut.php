d16dd723c40ca97b37b6d6374ab4cf6d0f5d6b2a
Yogev Heskia <Yogev911@gmail.com>
2017-02-18 18:21:08 +0200

    checkout fixed

--- CheckOut.php
+++ CheckOut.php
@@ -27,7 +27,8 @@
 <body>
 <!-- Nav -->
 <nav class="navbar navbar-inverse navbar-fixed-top">
-    <button type="button" class="navbar-toggle collapsed hamburger" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+    <button type="button" class="navbar-toggle collapsed hamburger" data-toggle="collapse" data-target="#navbar"
+            aria-expanded="false" aria-controls="navbar">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -51,7 +52,7 @@
             echo $val;
         }
         $userConnected = getUserObj(305166860);
-        echo '<h3><img class="userImg" src="'.$userConnected->pic.'">  '.$userConnected->name.'</h3>';
+        echo '<h3><img class="userImg" src="' . $userConnected->pic . '">  ' . $userConnected->name . '</h3>';
         ?>
         <a class="btn logInOutBtn"><span class="glyphicon glyphicon-log-out"></span></a>
     </section>
@@ -60,8 +61,8 @@
         <li><a href="recent.php"><i class="fa fa-history"></i> Recent</a></li>
         <li><a href="MakeYourOwn.php"><i class="fa fa-flask"></i> Customize</a></li>
         <li><a href="foryou.php"><i class="fa fa-user"></i> For you</a></li>
-        <li><a href="ourpicks.php"><span class="glyphicon glyphicon-glass" ></span> Our picks</a></li>
-        <li><a href="Trendy.php"><span class="glyphicon glyphicon-sunglasses" ></span> Trendy</a></li>
+        <li><a href="ourpicks.php"><span class="glyphicon glyphicon-glass"></span> Our picks</a></li>
+        <li><a href="Trendy.php"><span class="glyphicon glyphicon-sunglasses"></span> Trendy</a></li>
     </ul>
 </div>
 <!-- END Nav -->
@@ -69,69 +70,76 @@
     <h1><i class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></i>Check Out</h1>
 </header>
 <div class="alert alert-warning alert-dismissible" role="alert">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span>
+    </button>
     <strong><span class="glyphicon glyphicon-star"></span> </strong> Cocktail saved !
 </div>
 <div class="alert alert-danger alert-dismissible" role="alert">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span>
+    </button>
     <strong><span class="glyphicon glyphicon-warning-sign"></span> </strong> Cocktail already on favorites
 </div>
 <main class="wrapper">
-    <?php
-    if (empty($_GET["id"])) {
-        $cocktail = getCocktailPrices($_GET["alcoholOne"],$_GET["alcoholTwo"],$_GET["juiceOne"],$_GET["juiceTwo"]);
-        $cocktail->alcohol1_amount = $_GET["alcoholOneAmount"];
-        $cocktail->alcohol2_amount = $_GET["alcoholTwoAmount"];
-        $cocktail->juice1_amount = $_GET["juiceOneAmount"];
-        $cocktail->juice2_amount = $_GET["juiceTwoAmount"];
-        $cocktail->price = ($cocktail->alcohol1->price * $cocktail->alcohol1_amount/10) + ($cocktail->alcohol2->price * $cocktail->alcohol2_amount/10) + ($cocktail->juice1->price * $cocktail->juice1_amount/10) + ($cocktail->juice2->price * $cocktail->juice2_amount/10);
-//        $cocktail->price = 55;
-    }else{
-        $cocktail = getCocktailsByCocktailId($_GET["id"]);
-    }
-    echo '
-  <div class="panel panel-default">
-  <!-- Default panel contents -->
-      <div class="panel-heading"><u><b>Summery</b></u></div>
-      <table class="table table-hover">
-        <tr class="info">
-           <td>'.$cocktail->alcohol1->name.'</td>
-           <td>'.$cocktail->alcohol1_amount.'ml</td>
-           <td>'.$cocktail->alcohol1->price * $cocktail->alcohol1_amount/10 .'$ </td>
-        </tr>
-        <tr class="info">
-           <td>'.$cocktail->alcohol2->name.'</td>
-           <td>'.$cocktail->alcohol2_amount.'ml</td>
-           <td>'.$cocktail->alcohol2->price * $cocktail->alcohol2_amount/10 .'$</td>
-        </tr>
-        <tr class="info">
-           <td>'.$cocktail->juice1->name.'</td>
-           <td>'.$cocktail->juice1_amount.'ml</td>
-           <td>'.$cocktail->juice1->price * $cocktail->juice1_amount/10 .'$</td>
-        </tr>
-        <tr class="info">
-           <td>'.$cocktail->juice2->name.'</td>
-           <td>'.$cocktail->juice2_amount.'ml</td>
-           <td>'.$cocktail->juice2->price * $cocktail->juice2_amount/10 .'$</td>
-        </tr>
-        <tr class="success">
-           <td><b>Total</b></td>
-           <td></td>
-           <td><b>'.$cocktail->price.'$</b></td>
-        </tr>
-      </table>
-  </div>
-  <div class = "payPanel">
-      <div class="panel-heading">How Do You Wanna Pay?</div>
-    
-  <a href="#" class="list-group-item"><span class="fa fa-paypal"></span> PayPay</a>
-  <a href="#" class="list-group-item"><span class="fa fa-apple"></span> ApplePay</a>
-  <a href="#" class="list-group-item"><span class="fa fa-google-wallet"></span> GooglePay</a>
-  <a href="#" class="list-group-item"><span class="fa fa-credit-card-alt"></span> Credit Card  (highly Recommend)</a>
-</div></div>';
+    <div class="panel panel-default">
+        <!-- Default panel contents -->
+        <div class="panel-heading"><u><b>Summery</b></u></div>
+        <table class="table table-hover">
+            <?php
+            if (empty($_GET["id"])) {
+                $totalAlcohol = $_GET["alcoholOneAmount"] + $_GET["alcoholTwoAmount"];
+                $totalJuice = $_GET["juiceOneAmount"] + $_GET["juiceTwoAmount"];
 
-    disconnect();
-    ?>
+                $cocktail = getCocktailPrices($_GET["alcoholOne"], $_GET["alcoholTwo"], $_GET["juiceOne"], $_GET["juiceTwo"]);
+
+                if ($_GET["alcoholOne"] == $_GET["alcoholTwo"]) {
+                    $cocktail[0]->amount = $totalAlcohol;
+                    if ($_GET["juiceOne"] == $_GET["juiceTwo"]) {
+                        $cocktail[1]->amount = $totalJuice;
+                    } else {
+                        $cocktail[1]->amount = $_GET["juiceOneAmount"];
+                        $cocktail[2]->amount = $_GET["juiceTwoAmount"];
+                    }
+                } else {
+                    $cocktail[0]->amount = $_GET["alcoholOneAmount"];
+                    $cocktail[1]->amount = $_GET["alcoholTwoAmount"];
+                    if ($_GET["juiceOne"] == $_GET["juiceTwo"]) {
+                        $cocktail[2]->amount = $totalJuice;
+                    } else {
+                        $cocktail[2]->amount = $_GET["juiceOneAmount"];
+                        $cocktail[3]->amount = $_GET["juiceTwoAmount"];
+                    }
+                }
+                $price = 0;
+                foreach ($cocktail as $value) {
+                    $price += $value->price * $value->amount / 10;
+                }
+            } else {
+                $cocktail = getCocktailsByCocktailId($_GET["id"]);
+            }
+            foreach ($cocktail as $value) {
+                echo '<tr class="info">
+                        <td>' . $value->name . '</td>
+                        <td>' . $value->amount . 'ml</td>
+                        <td>' . $value->price * $value->amount / 10 . '$ </td>
+                      </tr>';
+            }
+            echo '<tr class="success">
+                    <td><b>Total</b></td>
+                    <td></td>
+                    <td><b>' . $price . '$</b></td>
+                  </tr>';
+            disconnect();
+            ?>
+        </table>
+    </div>
+    <div class="payPanel">
+        <div class="panel-heading">How Do You Wanna Pay?</div>
+        <a href="#" class="list-group-item"><span class="fa fa-paypal"></span> PayPay</a>
+        <a href="#" class="list-group-item"><span class="fa fa-apple"></span> ApplePay</a>
+        <a href="#" class="list-group-item"><span class="fa fa-google-wallet"></span> GooglePay</a>
+        <a href="#" class="list-group-item"><span class="fa fa-credit-card-alt"></span> Credit Card (highlyRecommend)</a>
+    </div>
+    </div>
 </main>
 <footer>
     <section class="contact">
